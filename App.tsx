4a982a021988fa1c387579411a2140179d2b716c
Hassanalihaider <hassanali80945@gmail.com>
2025-06-25 08:11:18 -0700

    work done

---
+++ App.tsx
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { NewAppScreen } from '@react-native/new-app-screen';
+<<<<<<< HEAD
+import { StatusBar, StyleSheet, useColorScheme, View,Text } from 'react-native';
+=======
+import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+>>>>>>> 546739b9b525d4fbe79277ad3755ceae279db8bb
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+<<<<<<< HEAD
+      {/* <NewAppScreen templateFileName="App.tsx" /> */}
+      <Text>
+        hey Github
+      </Text>
+=======
+      <NewAppScreen templateFileName="App.tsx" />
+>>>>>>> 546739b9b525d4fbe79277ad3755ceae279db8bb
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ src/navigators/navigator.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const navigator = () => {
+  return (
+    <View>
+      <Text>navigator</Text>
+    </View>
+  )
+}
+
+export default navigator
+
+const styles = StyleSheet.create({})
